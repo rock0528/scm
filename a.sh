d8d12a9222150a3dbd07198e8fa05d219e5c6141
fur5 <rock.fu@emc.com>
2016-01-07 14:49:33 +0800

    sdsad

---
+++ a.sh
@@ -0,0 +1,53 @@
+***************************************************************************
+                  Packaging Report for SOLARIS SPARC ALL
+                         Tue Dec 29 14:50:00 2015
+***************************************************************************
+Tue Dec 29 14:50:00 2015  [INFO]   Log File: /buildtools/logs/Packaging-1.3.83.1.0448-SOLARIS-SPARC-12292015.txt
+Tue Dec 29 14:50:18 2015  [INFO]   Object created for reBuildNumberVer module
+Tue Dec 29 14:50:18 2015  [INFO]   Cleaning up the existing stage directory /navi_shr/kitsStagingArea/sparc
+Tue Dec 29 14:50:21 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:50:21 2015  [INFO]   Packaging INITTOOL-SOLARIS-32-SPARC-EN_US-1.3.83.1.0448
+Tue Dec 29 14:50:21 2015  [INFO]   Creating the stage area by copying the packaging files from root directory
+Tue Dec 29 14:50:22 2015  [INFO]   Building package InitTool-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg....
+Tue Dec 29 14:50:26 2015  [INFO]   Verifying package InitTool-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg....
+Tue Dec 29 14:50:26 2015  [INFO]   Package build and verify successful
+Tue Dec 29 14:50:26 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:50:27 2015  [INFO]   Updated build number successfully
+Tue Dec 29 14:50:27 2015  [INFO]   Package move success
+Tue Dec 29 14:50:27 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:50:27 2015  [INFO]   Packaging NAVICLI-SOLARIS-32-SPARC-EN_US-7.33.83.4.48
+Tue Dec 29 14:50:27 2015  [INFO]   Creating the stage area by copying the packaging files from root directory
+Tue Dec 29 14:50:32 2015  [INFO]   Building package NaviCLI-Solaris-32-sparc-en_US-7.33.83.4.48-1.pkg....
+Tue Dec 29 14:50:56 2015  [INFO]   Verifying package NaviCLI-Solaris-32-sparc-en_US-7.33.83.4.48-1.pkg....
+Tue Dec 29 14:50:57 2015  [INFO]   Package build and verify successful
+Tue Dec 29 14:50:57 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:50:58 2015  [INFO]   Updated build number successfully
+Tue Dec 29 14:50:58 2015  [INFO]   Package move success
+Tue Dec 29 14:50:58 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:50:58 2015  [INFO]   Packaging HOSTAGENT-SOLARIS-32-SPARC-EN_US-1.3.83.1.0448
+Tue Dec 29 14:50:58 2015  [INFO]   Creating the stage area by copying the packaging files from root directory
+Tue Dec 29 14:50:59 2015  [INFO]   Building package HostAgent-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg....
+Tue Dec 29 14:51:08 2015  [INFO]   Verifying package HostAgent-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg....
+Tue Dec 29 14:51:08 2015  [INFO]   Package build and verify successful
+Tue Dec 29 14:51:08 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:51:09 2015  [INFO]   Updated build number successfully
+Tue Dec 29 14:51:09 2015  [INFO]   Package move success
+Tue Dec 29 14:51:09 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:51:09 2015  [INFO]   Packaging SERVERUTIL-SOLARIS-32-SPARC-EN_US-1.3.83.1.0448
+Tue Dec 29 14:51:09 2015  [INFO]   Creating the stage area by copying the packaging files from root directory
+Tue Dec 29 14:51:12 2015  [INFO]   Building package ServerUtil-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg....
+Tue Dec 29 14:51:37 2015  [INFO]   Verifying package ServerUtil-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg....
+Tue Dec 29 14:51:37 2015  [INFO]   Package build and verify successful
+Tue Dec 29 14:51:37 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:51:38 2015  [INFO]   Updated build number successfully
+Tue Dec 29 14:51:38 2015  [INFO]   Package move success
+Tue Dec 29 14:51:38 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:51:38 2015  [INFO]   SUCCESS: /navi_shr/kits/solaris/1.3.83.1.0448/InitTool-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg
+Tue Dec 29 14:51:38 2015  [INFO]   SUCCESS: /navi_shr/kits/solaris/1.3.83.1.0448/NaviCLI-Solaris-32-sparc-en_US-7.33.83.4.48-1.pkg
+Tue Dec 29 14:51:38 2015  [INFO]   SUCCESS: /navi_shr/kits/solaris/1.3.83.1.0448/HostAgent-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg
+Tue Dec 29 14:51:38 2015  [INFO]   SUCCESS: /navi_shr/kits/solaris/1.3.83.1.0448/ServerUtil-Solaris-32-sparc-en_US-1.3.83.1.0448-1.pkg
+Tue Dec 29 14:51:38 2015  [INFO]   ----------------------------------------------------------
+Tue Dec 29 14:51:38 2015  [INFO]   View Log file /buildtools/logs/Packaging-1.3.83.1.0448-SOLARIS-SPARC-12292015.txt
+Tue Dec 29 14:51:38 2015  [INFO]   Use debug mode for detailed information
+
+Completed at Tue Dec 29 14:51:38 2015
